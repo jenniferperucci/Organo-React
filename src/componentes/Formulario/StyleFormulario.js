75d0d4c024c1d8c5203d125b76e9d26c8c26309f
Jennifer Perucci de Almeida <jenniferperucci@hotmail.com>
2023-02-16 22:11:31 -0300

    Início-Responsividade

--- src/componentes/Formulario/StyleFormulario.js
+++ src/componentes/Formulario/StyleFormulario.js
@@ -4,6 +4,9 @@ export const Formularios = styled.section`
     display: flex;
     justify-content: center;
     margin: 80px 0;
+    @media (min-width:375px) {
+        margin: 45px 0;
+    }
     form{
     max-width: 80%;
     background-color: #F2F2F2;
@@ -11,6 +14,7 @@ export const Formularios = styled.section`
     padding: 36px 64px;
     box-shadow: 8px 8px 16px rgba(0,0,0, 0.08);
     }
+    
 `;
 
 export const Forms = styled.form`
